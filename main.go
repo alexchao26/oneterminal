167b30e13f6ac931ab6490168eeda0a06872ba66
Alex Chao <alexchaoo@gmail.com>
2021-03-20 15:28:54 -0400

    auto update version via goreleaser (#8)

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "github.com/alexchao26/oneterminal/internal/cli"
+import (
+	"fmt"
+	"os"
+
+	"github.com/alexchao26/oneterminal/internal/cli"
+)
+
+// variable is updated by goreleaser automatically
+var version = "dev"
 
 func main() {
-	cli.Execute()
+	rootCmd, err := cli.Init(version)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	err = rootCmd.Execute()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
